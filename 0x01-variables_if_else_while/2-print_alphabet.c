071caa1936a94b4a9786e0539dc1ad3f785eb542
mweroshanny <mweroshannyy@gmail.com>
2023-07-17 02:45:45 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int l_case;
+
+	for (l_case = 'a'; l_case <= 'z'; l_case++)
+	{
+		putchar(l_case);
+
+	}
+		putchar('\n');
+	return (0);
+}
