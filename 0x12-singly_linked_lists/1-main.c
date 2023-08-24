48065cb1a18548a424ee913ff1530918d951af50
mweroshanny <mweroshannyy@gmail.com>
2023-08-24 14:19:32 -0700

    1-main.c

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -1,4 +1,7 @@
-#include "variadic_functions.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
  * main - check the code
@@ -7,6 +10,25 @@
  */
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
     return (0);
 }
